8e4f0ef50778a4e3e3d39a70a0fd376e40bfc07c
Paul <mothcar@gmail.com>
2024-05-08 15:39:30 +0900

    add input

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,27 @@
-
+import { useState } from "react";
 export default function Home() {
+  const [name, setName] = useState("");
+
+  function handleChange(event) {
+    setName(event.target.value)
+    // console.log('Name : ', name)
+  }
+
+  function handleSubmit(event) {
+    alert('A name was submitted: ' + name);
+    event.preventDefault();
+  }
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>Home</div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name: 
+          <input name="myInput" value={name} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  );
 }
